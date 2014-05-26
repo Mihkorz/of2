6a2fd52d36fc9d2d8641b80f535dacd2db52a744
mikhail-smolenskaya <slim002@yandex.ru>
2014-05-26 17:46:54 +0400

    calculation form

--- core/forms.py
+++ core/forms.py
@@ -3,11 +3,11 @@
 from django import forms
 
 class CalculationParametersForm(forms.Form):
-    sigma_num = forms.FloatField( label="Sigma ammount", initial=2)
-    use_sigma = forms.BooleanField(label="Use sigma filter", initial=True)
-    cnr_low = forms.FloatField(label="CNR lower limit", initial=0.67)
-    cnr_up = forms.FloatField(label="CNR upper limit", initial=1.5)
-    use_cnr = forms.BooleanField(label="Use CNR filter", initial=True)
+    sigma_num = forms.FloatField( label="Sigma ammount", initial=2, required=False)
+    use_sigma = forms.BooleanField(label="Use sigma filter", initial=True, required=False)
+    cnr_low = forms.FloatField(label="CNR lower limit", initial=0.67, required=False)
+    cnr_up = forms.FloatField(label="CNR upper limit", initial=1.5, required=False)
+    use_cnr = forms.BooleanField(label="Use CNR filter", initial=True, required=False)
     
     def __init__(self, *args, **kwargs):
         super(CalculationParametersForm, self).__init__(*args, **kwargs)
