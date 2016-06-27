c10ab33b4ef3f4e26a067afecdc7d55dd678a07b
Mihkorz <mihkorz@gmail.com>
2016-06-27 19:05:35 +0300

    update eport portal4

--- report/views.py
+++ report/views.py
@@ -1074,11 +1074,12 @@ class ReportAjaxPathDetail(TemplateView):
         gene_df = pd.DataFrame(gene_data).set_index('SYMBOL')
                
         df_file_cnr = pd.read_csv(group.doc_logfc.path, index_col='SYMBOL')
-        
+        """
         if df_file_cnr['adj.P.Val'].all() == 1:
-                    df_file_cnr = df_file_cnr[(np.absolute(df_file_cnr['logFC'])>1)]
+                    df_file_cnr = df_file_cnr[(np.absolute(df_file_cnr['logFC'])>2)]
         else:        
             df_file_cnr = df_file_cnr[(df_file_cnr['adj.P.Val']<0.05) & (np.absolute(df_file_cnr['logFC'])>0.4)] 
+        """
         df_file_cnr = df_file_cnr['logFC'].round(decimals=2)
         
         
