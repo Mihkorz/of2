be0371f625408e218ab9671de6e9320f9979a96f
Mihkorz <mihkorz@gmail.com>
2016-11-10 20:18:31 +0300

    add maxim_format_pathway and convert_human_pathway_to_mouse functions to core.util_views.py

--- core/utils_views.py
+++ core/utils_views.py
@@ -33,6 +33,188 @@ class ConvertPath(TemplateView):
         return super(ConvertPath, self).dispatch(request, *args, **kwargs)
         
     
+    def maxim_format_pathway(self, filepath, organism, database):
+        """
+        Add new pathway to OF database 
+        using Maxim pathway file format
+        Format: Excel file with corresponding sheetnames
+        
+        All code with 'apply' functions is wrapped in try-except for debugging reasons if needed
+        """
+        
+        file_name = filepath.split('/')[-1] # split filepath and get last element as filename
+        pathname = file_name.replace('.xls', '').replace(' ', '_') # get pathname from filename
+        
+        try: #check if pathway already exists
+            new_path = Pathway.objects.get(name=pathname, organism=organism, database=database )               
+        except: #if not, add new
+            new_path = Pathway(name=pathname, amcf=0, organism=organism, database=database)
+            new_path.save()
+        
+        
+        ##############################################################################    
+        #Add Genes
+        ##############################################################################        
+            
+        def add_gene(row, path):
+            if row['arr']!='missing':
+                g = Gene(name=row['gene'], arr=row['arr'], pathway=path)
+                g.save()                
+           
+        try:
+            df_genes = read_excel(filepath, sheetname='genes', header=None).fillna('missing')
+            df_genes.columns = ['gene', 'arr']
+            df_genes.apply(add_gene, axis=1, path=new_path)
+        except:
+            pass 
+        
+        ##############################################################################    
+        #Add Nodes and Components. Update Node names
+        ##############################################################################  
+        
+        def add_node_and_components(row, path):
+                
+            node_name = row.name
+            try:
+                new_node = Node.objects.get(name=node_name, pathway=path)
+            except:
+                new_node = Node(name=node_name, pathway=path)
+                new_node.save()
+            row = row[row!=1]
+            row.dropna(inplace=True) # taking into account different row lengths for different nodes
+            for component in row:
+                new_component = Component(name=component, node=new_node)
+                new_component.save()
+            
+        try:
+            df_nodes = read_excel(filepath, sheetname='nodes', header=None, index_col=0)
+            df_nodes.drop(1, axis=1, inplace=True)
+            df_nodes_name = df_nodes[2]
+            
+            #Node name = name of the first component in row
+            #see file for more details
+            
+            df_nodes.apply(add_node_and_components, axis=1, path=new_path)
+        except:
+            pass
+        
+        
+        # Update Node names
+        
+        def update_node_name(row, path):
+            name = row.name
+            normal_name = row['new name']
+            
+            node = Node.objects.get(name=name, pathway=path)
+            node.name = normal_name
+            node.save()        
+        
+        try:
+            df_node_names = read_excel(filepath, sheetname='node_names', header=None, index_col=0)
+            df_node_names.columns = ['new name']       
+            df_node_names.apply(update_node_name, axis=1, path=new_path)
+        except:
+            pass
+        
+        ##############################################################################    
+        #Add Relations
+        ############################################################################## 
+        
+        def add_relation(row, sNodes, path):
+            from_node_name = row['from']
+            to_node_name = row['to']
+            
+            reltype = 2 # default value=2(unknown) to draw black arrows
+            if row['reltype']=='activation':
+                reltype = 1
+            if row['reltype']=='inhibition':
+                reltype = 0
+            if row['reltype']=='unknown':
+                reltype = 2
+                    
+            db_node_from = Node.objects.get(name=from_node_name, pathway=path)
+            db_nodeto = Node.objects.get(name=to_node_name, pathway=path)
+                
+            nrel = Relation(fromnode=db_node_from, tonode=db_nodeto, reltype=reltype)
+            nrel.save()
+            
+        
+        try:
+            df_rels = read_excel(filepath, sheetname='edges', header=None)
+            df_rels.columns = ['from', 'to', 'reltype']
+            df_rels.apply(add_relation, axis=1, sNodes=df_nodes_name, path=new_path)    
+        except:
+            pass               
+            
+        
+    def convert_human_pathway_to_mouse(self, database, delete_old):
+        """
+        Function to convert Human Pathways to Mouse. 
+        For the whole database at once
+        
+        delete_old - trigger to delete old pathways from corresponding mouse database
+        
+        """
+        
+        if delete_old:
+            for mouse_path in Pathway.objects.filter(organism='mouse', database=database):
+                mouse_path.delete()
+        
+        human_paths = Pathway.objects.filter(organism='human', database=database)
+        i=0
+        for human_path in human_paths:
+            i=i+1
+            print human_path.name+' i = '+str(i) #to see progress in console
+            
+            try:
+                mouse_path = Pathway.objects.get(organism='mouse', database=database, name=human_path.name)
+            except:
+                mouse_path = Pathway(organism='mouse', database=database, name=human_path.name, amcf=human_path.amcf)
+                mouse_path.save()
+            
+            
+            #Add genes
+            for human_gene in human_path.gene_set.all():
+                try:
+                    mapping = MouseMapping.objects.filter(human_gene_symbol=human_gene.name)[0] #get first occurrence
+                
+                    mouse_gene = Gene(name=mapping.mouse_gene_symbol.upper(), #make gene name uppercase 
+                                       arr=human_gene.arr, pathway=mouse_path,
+                                       comment=human_gene.comment)
+                except: # if no match found in Mapping table
+                    mouse_gene = Gene(name=human_gene.name, arr=human_gene.arr, pathway=mouse_path,
+                               comment=human_gene.comment)
+                    
+                mouse_gene.save()
+            
+            #Add Nodes and Components    
+            for human_node in human_path.node_set.all():
+                mouse_node = Node(name=human_node.name, comment=human_node.comment, pathway=mouse_path)
+                mouse_node.save()
+                
+                for human_component in human_node.component_set.all():
+                    try:
+                        mapping = MouseMapping.objects.filter(human_gene_symbol=human_component.name)[0]
+                        mouse_component = Component(name=human_component.mouse_gene_symbol.upper(), node=mouse_node)
+                        
+                    except:
+                        mouse_component = Component(name=human_node.name, node=mouse_node)
+                    mouse_component.save()
+                    
+            #Add relations
+            for human_node in human_path.node_set.all():
+                for human_inrel in human_node.inrelations.all():
+                    
+                    mouse_from_node = Node.objects.filter(name=human_inrel.fromnode.name, pathway=mouse_path)[0]
+                    mouse_to_node = Node.objects.filter(name=human_inrel.tonode.name, pathway=mouse_path)[0]
+                    
+                    mouse_relation = Relation(fromnode=mouse_from_node,
+                                               tonode=mouse_to_node,
+                                               reltype = human_inrel.reltype)
+                    mouse_relation.save()    
+        
+    
+    
     def get_context_data(self, **kwargs):
         context = super(ConvertPath, self).get_context_data(**kwargs)
         
