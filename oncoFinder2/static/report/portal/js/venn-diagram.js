27c3bdac382028d81a2c46d1f8482d82cca24d46
Mihkorz <mihkorz@gmail.com>
2017-02-08 16:21:23 +0300

    add Deep learning to gsk_l1000 7

--- oncoFinder2/static/report/portal/js/venn-diagram.js
+++ oncoFinder2/static/report/portal/js/venn-diagram.js
@@ -195,6 +195,9 @@ function drawVenn(reportID, renderTo,
 
                    // Display a tooltip with the current size
                    tooltip.transition().duration(400).style("opacity", .9);
+                   
+                   if(path_gene == 'deeplearning') path_gene = 'genes;'
+                   
                    tooltip.text(d.size + " "+path_gene);
                    
 
@@ -229,6 +232,70 @@ function drawVenn(reportID, renderTo,
 			);
 	
 }
+
+
+function drawVennWithData(reportID, renderTo, tolltip, data){
+	
+	
+		var sets = data;
+		
+		$("#"+renderTo).empty();
+
+        var div = d3.select("#"+renderTo);
+        div.datum(sets).call(venn.VennDiagram()
+        		                              .width(400)
+                                              .height(400));
+        
+     // add a tooltip          
+        var tooltip = d3.select("body").append("div").attr("class", "venntooltip");
+        div.selectAll("path")
+        .style("stroke-opacity", 0)
+        .style("stroke", "#fff")
+        .style("stroke-width", 0);
+     // add listeners to all the groups to display tooltip on mousover
+     div.selectAll("g")
+         .on("mouseover", function(d, i) {
+           // sort all the areas relative to the current item
+           venn.sortAreas(div, d);
+
+           // Display a tooltip with the current size
+           tooltip.transition().duration(400).style("opacity", .9);
+           
+           
+           
+           tooltip.text(d.size + " "+tolltip);
+           
+
+           // highlight the current path
+           var selection = d3.select(this).transition("tooltip").duration(400);
+           selection.select("path")
+                                  .style("stroke-width", 3)
+                                  .style("fill-opacity", d.sets.length == 1 ? .4 : .1)
+                                  .style("stroke-opacity", 1);
+                                          })
+         .on("mousemove", function() {
+                          tooltip.style("left", (d3.event.pageX) + "px")
+                                 .style("top", (d3.event.pageY - 28) + "px");
+                                      })
+         .on("mouseout", function(d, i) {
+              tooltip.transition().duration(400).style("opacity", 0);
+              var selection = d3.select(this).transition("tooltip").duration(400);
+              selection.select("path")
+                       .style("stroke-width", 0)
+                       .style("fill-opacity", d.sets.length == 1 ? .25 : .0)
+                       .style("stroke-opacity", 0);
+                                         })
+         .on("click", function(d, i){
+        	
+        	 venn.sortAreas(div, d);
+             var idx = d.id;
+             
+             //rawDynamicTable(reportID, idx, tblRenderTo, path_gene, categories, organism, is_barplot, is_metabolic);
+        	 
+         });
+	}
+	
+
 
 $(document).ready(function(){
 	/*
