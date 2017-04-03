dbb2ce910b65de5703b0a9977960e9ea1dd9b9b3
mihkorz <mihkorz@gmail.com>
2017-04-04 01:04:53 +0300

    add Novartis user 1

--- report/views.py
+++ report/views.py
@@ -77,6 +77,9 @@ class ReportDetail(DetailView):
                 self.template_name = 'report/gsk_prj2_4d_medium_low.html'
         if 'nova' in self.get_object().slug and (self.is_member(user, 'Novartis') or user.is_staff):
             self.template_name = 'report/report_detail_abovebeyond.html'
+            
+        if 'nova_prj1_biochem' in self.get_object().slug and (self.is_member(user, 'Novartis') or user.is_staff):
+            self.template_name = 'report/just_text.html'
         
                 
                 
